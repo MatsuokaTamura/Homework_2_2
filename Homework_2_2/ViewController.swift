c4f68e19675cfb7107aca61e6f52ac43ca299fe8
matsuoka_tamura <sam-anas2@yandex.ru>
2022-02-01 10:59:12 +0300

    Initial Commit

---
+++ Homework_2_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2_2
+//
+//  Created by Matsuoka Tamura on 01.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
