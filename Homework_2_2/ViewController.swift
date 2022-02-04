a54a43b035497c0bd595dc94efef947b37774304
matsuoka_tamura <sam-anas2@yandex.ru>
2022-02-04 16:51:39 +0300

    HomeWork 2.2 complete

--- Homework_2_2/ViewController.swift
+++ Homework_2_2/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Сначала вставляем вьюшечку
+    @IBOutlet var colorView: UIView!
+    
+    // После чего добавляем наши лейблы-значения
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    // И уже затем вносим 1 часть слайдеров как аутлетов
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    // Далее идут функции
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // Задаем радиус вьюшке
+        colorView.layer.cornerRadius = 25
+        
+        // Задаем цвета с левой стороны у слайдеров
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        // Поскольку у нас автоматически и по умолчанию голубой, то этот код не обязателен - blueSlider.minimumTrackTintColor = .blue
+        
+        // Добавляем цвета в вью через приватный 1 метод
+        setsOfColors()
+        
+        
+        // Теперь совмещаем значения лейбла в зависимости от слайдера и округляем до сотых по 2f через приватный 2 метод
+        labelInValueSlider()
     }
 
+    // Создаем единый экшн ко всем слайдерам
+    @IBAction func RedGreenBlueSlider(_ sender: UISlider) {
+        setsOfColors()
+        
+        labelInValueSlider()
+    }
+    
+    //
+    private func setsOfColors() {
+        colorView.backgroundColor = UIColor (
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    //Теперь совмещаем значения лейбла в зависимости от слайдера и округляем до сотых по 2f
+    private func labelInValueSlider() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 
 }
 
